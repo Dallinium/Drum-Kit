625482452245928ae3f38059f0481e524f908a53
SochendaS <dallinium@gmail.com>
2022-07-21 21:00:12 -0700

    added event as paramenter

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-
-
 for (var i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
   });
 }
 
-document.addEventListener("keydown", function(){
+document.addEventListener("keydown", function (event) {
   makeSound(event.key);
   buttonAnimation(event.key);
-})
+});
 
-function makeSound(key){
-
+function makeSound(key) {
   switch (key) {
     case "w":
       var crash = new Audio("sounds/crash.mp3");
@@ -55,16 +52,14 @@ function makeSound(key){
 
     default:
       console.log("doesn't work");
-
   }
-};
+}
 
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector("." + currentKey);
   activeButton.classList.add("pressed");
 
   setTimeout(function () {
-    activeButton.classList.remove("pressed")
-  }
-  , 100);
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
